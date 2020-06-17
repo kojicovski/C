b8d254b5c7ed07b79f1fb86688aeb955362163ac
Samuel Kojicovski <kojicovski@gmail.com>
2020-06-16 21:37:33 -0300

    pointers concepts

--- pointers/firstExamplePointers.c
+++ pointers/firstExamplePointers.c
@@ -5,11 +5,21 @@ int main () {
 
 int x = 10;
 int *p;
-
 p = &x;
+//ponteiro p aponta para variavel x
 
 printf("x = %d\n",x);
 printf("&x = %d\n",&x);
 printf("p = %d\n",p);
+printf("*p = %d\n",*p);
+
+*p = 12; //modificar conteudo da onde meu ponteiro está apontando
+printf("x = %d\n",x);
+printf("*p = %d\n",*p);
+printf("p = %d\n",p);
+printf("&x = %d\n",&x);
+
+system("pause");
+return 0;
 
 }
