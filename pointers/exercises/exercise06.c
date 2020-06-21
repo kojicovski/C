58315ad776377f52baea73cb81862dbd4d4dacfe
Samuel Kojicovski <kojicovski@gmail.com>
2020-06-21 20:01:46 -0300

    Exercise 06

---
+++ pointers/exercises/exercise06.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*Fac¸a um programa que leia dois valores inteiros e chame uma func¸ ˜ao que receba estes
+2 valores de entrada e retorne o maior valor na primeira vari ´avel e o menor valor na
+segunda vari ´avel. Escreva o conte´udo das 2 vari ´aveis na tela.
+*/
+
+int main() {
+
+    int a,b;
+    printf("valor para a:");
+    scanf("%d",&a);
+    printf("Valor para b:");
+    scanf("%d",&b);
+    printf("Chamando funcao dobro\n");
+    dobro(&a,&b);
+    printf("valor para a: %d e b: %d\n",a,b);
+
+system("PAUSE");
+return 0;
+}
+
+void dobro (int *i, int *j) {
+    *i = 2 * *i;
+    *j = 2 * *j;
+
+return;
+}
